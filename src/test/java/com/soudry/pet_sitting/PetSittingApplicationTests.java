5800c7ce45ae00c29c4d7ce09903554be4b26678
Alexander Soudry <soudryalex@gmail.com>
2026-08-08 17:55:36 -0400

    initial commit

---
+++ src/test/java/com/soudry/pet_sitting/PetSittingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soudry.pet_sitting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSittingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
